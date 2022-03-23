92cf6f619cc4a66f9d6432722af88f43a2cb40f9
Luiz Felipe da Silva Valério <luiz.valerio@fatec.sp.gov.br>
2022-03-23 20:22:13 -0300

    Update app.js
    Added jump logic

--- app.js
+++ app.js
@@ -5,11 +5,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let birdLeft = 220
     let birdBottom = 100
+    let birdTop = birdBottom + 45
+    let gravity = 2
 
 
     function startGame() {
+        birdBottom -= gravity
         bird.style.bottom = birdBottom + 'px'
         bird.style.left = birdLeft + 'px'
     }
-    startGame()
+    let timerId = setInterval(startGame, 20)
+
+    function control(e) {
+        if (e.keyCode === 32) {
+            jump()
+        }
+    }
+
+
+    function jump() {
+        if (birdBottom < 455) birdBottom += 50
+        bird.style.bottom = birdBottom + 'px'
+        console.log(birdBottom)
+    }
+    document.addEventListener('keyup', control)
+
+
+    function generalObstacle (){
+        const obstacle = document.createElement('div')
+        obstacle.classList.add('obstacle')
+    }
 })
